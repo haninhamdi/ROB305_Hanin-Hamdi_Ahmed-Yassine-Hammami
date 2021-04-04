6a9d5169c973815df71567b23c0be3906f5502ec
Hanin Hamdi <71626538+haninhamdi@users.noreply.github.com>
2021-04-04 14:34:26 +0200

    Create PosixThread.cpp

---
+++ TD4/PosixThread.cpp
@@ -0,0 +1,89 @@
+#include "PosixThread.h"
+#include "/home/hani.hamdi/ROB305/TD1/time_library.cpp"
+
+using namespace std ; 
+char* PosixThread::Exception::throwException()
+{
+  throw "Posix Thread doesn't exit" ;
+}
+
+PosixThread::PosixThread()
+{
+  isActive = false ;
+  pthread_attr_init(&posixAttr) ;
+}
+
+PosixThread::PosixThread(pthread_t posixId) 
+{
+  this->posixId = posixId ;
+  isActive = true ;
+}
+
+PosixThread::~PosixThread()
+{
+  pthread_attr_destroy(&posixAttr) ;
+  cout<<"---PosixThread destroyed----"<<endl ; 
+}
+
+void PosixThread::start(void* (*threadFunc) (void*), void* threadArg)
+{
+  if (not(isActive))
+  {
+    pthread_create(&posixId, &posixAttr, threadFunc, threadArg) ;
+    isActive = true ;
+  }
+}
+
+void PosixThread::join()
+{
+  pthread_join(posixId, nullptr) ; 
+}
+
+bool PosixThread::join(double timeout_ms)
+{
+  timespec timeout_ts = timespec_from_ms(timeout_ms) ;
+  int error = pthread_timedjoin_np(posixId, nullptr, &timeout_ts) ; 
+  if (error == 0) /* thread finished before the timeout*/
+    return true ;
+  else
+    return false ;
+}
+
+bool PosixThread::setScheduling(int schedPolicy , int priority)
+{
+  pthread_attr_setinheritsched(&posixAttr, PTHREAD_EXPLICIT_SCHED) ;
+
+  sched_param schedParams ;
+  schedParams.sched_priority = priority ;
+  if (isActive) /* Active thread */
+  {
+    pthread_setschedparam(posixId, schedPolicy, &schedParams) ;
+    return true ;
+  }
+  else /* Non active thread */
+  {
+    pthread_attr_setschedpolicy(&posixAttr, schedPolicy) ;
+    pthread_attr_setschedparam(&posixAttr, &schedParams) ;
+    return false ;
+  }
+}
+
+bool PosixThread::getScheduling(int* p_schedPolicy , int* p_priority )
+{
+  int error , error_1 ;
+  sched_param schedParams ;
+  schedParams.sched_priority = *p_priority ;
+  if (isActive)
+  {
+    error = pthread_getschedparam(posixId, p_schedPolicy, &schedParams) ;
+    
+    return true ;
+  }
+  else
+  {
+    error_1 = pthread_attr_getschedparam(&posixAttr, &schedParams) ;
+    error_1 = pthread_attr_getschedpolicy(&posixAttr, p_schedPolicy) ;
+
+    return false ;
+  }
+}
