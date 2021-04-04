42d2d83b05c62a9651d3d78d062013c13e472146
ahmedyassine-hammami <57714752+ahmedyassine-hammami@users.noreply.github.com>
2021-04-04 22:08:27 +0100

    Add files via upload

---
+++ TD5/Mutex.h
@@ -0,0 +1,71 @@
+#ifndef Mutex_h_INCLUDED 
+#define Mutex_h_INCLUDED 
+
+#include <time.h> 
+#include <pthread.h>
+////////////////////////////////////////////////////////////////
+//////////////// Declaration of Mutex class ////////////////////
+////////////////////////////////////////////////////////////////
+class Mutex
+{
+    public :
+       Mutex() ; 
+       Mutex(bool isInversionSafe) ; 
+       ~Mutex() ; 
+       class Monitor ;
+       class Lock;
+       class TryLock;
+    protected :
+       pthread_mutex_t posixId ;
+       pthread_cond_t posixCondId;
+       bool lock();
+       bool lock(double timeout_ms);
+       bool trylock();
+       bool unlock();
+};
+
+////////////////////////////////////////////////////////////////
+//////////////// Declaration of Monitor class //////////////////
+////////////////////////////////////////////////////////////////
+class Mutex::Monitor
+{
+    public :
+      class TimeoutException;
+      void wait();
+      bool wait(double timeout_ms);
+      void notify();
+      void notifyAll();
+      Monitor(Mutex& m); 
+      Mutex& mutex_ ; 
+};
+////////////////////////////////////////////////////////////////
+////////////// Declaration of TimeoutException class ///////////
+////////////////////////////////////////////////////////////////
+class Mutex::Monitor::TimeoutException
+{
+   public :
+     char* throwException() ;
+}; 
+////////////////////////////////////////////////////////////////
+///////////////// Declaration of Lock class ////////////////////
+////////////////////////////////////////////////////////////////
+class Mutex::Lock : Mutex::Monitor 
+{
+    public :
+        Lock(Mutex &m);
+        Lock(Mutex &m, double timeout_ms) ;
+       ~Lock();   
+};
+
+////////////////////////////////////////////////////////////////
+//////////////// Declaration of TryLock class //////////////////
+////////////////////////////////////////////////////////////////
+class Mutex::TryLock : public Mutex::Monitor
+{
+    public :
+       TryLock( Mutex &m) ;
+       ~TryLock();
+     
+};
+
+#endif 
