e22d78e4be2873403e658e8009a5a1780d11b7b4
Hanin Hamdi <71626538+haninhamdi@users.noreply.github.com>
2021-04-04 14:34:59 +0200

    Create Thread.h

---
+++ TD4/Thread.h
@@ -0,0 +1,28 @@
+#ifndef Thread_h_INCLUDED
+#define Thread_h_INCLUDED
+
+#include "PosixThread.h" 
+
+class Thread : public PosixThread 
+{
+   private : 
+     double startTime_ms_ ; 
+     double stopTime_ms_ ;
+     pthread_t posixId ;
+     pthread_attr_t posixAttr ; 
+   public :
+     Thread() ; 
+     ~Thread() ; 
+     void start() ; 
+     double startTime_ms() ;
+     double stopTime_ms() ;
+     void stopTime() ; 
+     double execTime_ms() ; 
+     static void sleep_ms(double delay_ms) ; 
+   protected :
+     virtual void run() = 0 ;
+   private :
+       static void* call_run(void* v_thread) ;    
+};
+
+#endif 
