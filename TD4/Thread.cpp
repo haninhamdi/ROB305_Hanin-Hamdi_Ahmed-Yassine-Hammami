77b7ed5df37961a289d91f278bc2927f409790b4
Hanin Hamdi <71626538+haninhamdi@users.noreply.github.com>
2021-04-04 14:35:34 +0200

    Create Thread.cpp

---
+++ TD4/Thread.cpp
@@ -0,0 +1,63 @@
+#include "Thread.h" 
+#include <time.h>
+#include "PosixThread.cpp"
+
+using namespace std ; 
+
+Thread::Thread() : PosixThread()
+{
+   cout<<"----Init thread -----"<<endl ; 
+}
+
+Thread::~Thread() 
+{
+}
+
+void Thread::start() 
+{
+  cout<<"---Starting Thread ---"<<endl ; 
+  /* Set startTime to current time */ 
+  timespec startTime_ts = timespec_now(); 
+  startTime_ms_ = timespec_to_ms(startTime_ts) ; 
+  
+  cout<<"Waiting ..."<<endl ; 
+  PosixThread::start(call_run, (void*) this) ; 
+  cout<<"----Thread started ----"<<endl ; 
+}
+
+double Thread::startTime_ms()
+{
+  return startTime_ms_ ; 
+}
+
+double Thread::stopTime_ms()
+{
+  return stopTime_ms_ ; 
+}
+
+void Thread::stopTime() 
+{
+  timespec stopTime_ts ;
+  stopTime_ts = timespec_now(); 
+  stopTime_ms_ = timespec_to_ms(stopTime_ts) ; 
+}
+
+double Thread::execTime_ms()
+{
+  return stopTime_ms_ - startTime_ms_ ; 
+}
+
+void Thread::sleep_ms(double delay_ms)
+{
+  timespec delay_ts = timespec_from_ms(delay_ms) ;
+  cout<<"----Start sleep for : "<<delay_ms<<"  ms---"<<endl ; 
+  timespec_wait(delay_ts) ; 
+  cout<<"----Finished sleep for : "<<delay_ms<<"  ms---"<<endl ; 
+}
+
+void* Thread::call_run(void* v_thread)
+{ 
+   Thread* pThread = (Thread*) v_thread ;
+   pThread->run() ;
+   return v_thread ; 
+}
